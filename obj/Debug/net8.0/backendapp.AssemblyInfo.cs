d901d7469e54e0bd24aed98503084bc482a5a50f
ngochong114 <vothingoc.hong1104@gmail.com>
2025-11-20 13:38:17 +0700

    commit

--- obj/Debug/net8.0/backendapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/backendapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6b585d0bbc497f2a434d6462a1a4469470dda4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
