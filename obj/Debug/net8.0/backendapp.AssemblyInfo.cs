842c328061adfc5e44ba30bd4d1db07f48581d86
Cuongne2406 <cainnguyen2005@gmail.com>
2025-11-20 12:09:36 +0700

    done basic front end

--- obj/Debug/net8.0/backendapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/backendapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6b585d0bbc497f2a434d6462a1a4469470dda4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d0e2f83f37bc037c700db2e8c7129860093c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
